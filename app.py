a6e1850aef1adcb45b5bd85cfe90605b9bf0f276
Zac <zac@identityworksllc.com>
2022-07-23 11:58:22 -0500

    API Changes: - Added base desk functions for MacOS and Linux - Currently only returning string messages

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 # flask api for desk control and monitoring
 
+import json
+from turtle import up
+from urllib import response
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+import sys
 
 app = Flask(__name__)
 
@@ -24,7 +28,7 @@ class Hello(Resource):
     def post(self):
           
         data = request.get_json()     # status code
-        return jsonify({'data': data}), 201
+        return jsonify({'data': data})
   
   
 # another resource to calculate the square of a number
@@ -37,13 +41,32 @@ class Square(Resource):
 class Desk(Resource):
     
     def get(self):
-        return jsonify({'message': 'This is the current desk height'})
+        print(request)
+        if sys.platform != 'linux':
+            response = jsonify({'height':'MacOS: 50'})
+            response.status_code = 200
+            return response
+        else:
+            response = jsonify({'height': 'Linux: 50'})
+            response.status_code = 200
+            return response
     
     def post(self):
-        print("received post request")
-        print(request)
-        print(request.get_data())
-        return 201
+        data = request.get_json()
+        print(data['function'])
+        if sys.platform != 'linux':
+            resp = jsonify({'message': str('MacOS: desk going ' + data['function'])})
+            resp.status_code = 201
+            return resp
+        else:
+            resp = jsonify({'message': str('Linux: desk going ' + data['function'])})
+            resp.status_code = 201
+            return resp
+
+        # print("received post request")
+        # print(request)
+        # print(request.get_data())
+        # return 201
         
 # adding the defined resources along with their corresponding urls
 api.add_resource(Hello, '/')
@@ -54,4 +77,4 @@ api.add_resource(Desk, '/desk')
 # driver function
 if __name__ == '__main__':
   
-    app.run(debug = True)
+    app.run(debug = True, host='0.0.0.0', port='5001')
